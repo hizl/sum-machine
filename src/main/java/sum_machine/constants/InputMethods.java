bce0aa12a4d18e4d34ca06191c0597632d48a753
Lebed-kun <lebed-kun@mail.ru>
2020-09-12 21:37:25 +0300

    Add controlled input

---
+++ src/main/java/sum_machine/constants/InputMethods.java
@@ -0,0 +1,6 @@
+package src.main.java.sum_machine.constants;
+
+public final class InputMethods {
+    public final static String CONSOLE = "console";
+    public final static String FILE = "file";
+}
---
+++ src/main/java/sum_machine/io/ControlledInput.java
@@ -0,0 +1,64 @@
+package src.main.java.sum_machine.io;
+
+import java.util.HashMap;
+import java.util.regex.Pattern;
+
+import src.main.java.sum_machine.utils.Observable;
+import src.main.java.sum_machine.utils.Observer;
+import src.main.java.sum_machine.constants.Events;
+import src.main.java.sum_machine.constants.InputMethods;
+import src.main.java.sum_machine.utils.Pair;
+
+public class ControlledInput implements Input, Observable {
+    private HashMap<String, Input> inputs;
+    private String method = InputMethods.CONSOLE;
+
+    private void subscribeToInputChange(Observer observer) {
+        observer.subscribe(
+            Events.CHANGE_INPUT_METHOD,
+            this
+        );
+    }
+
+    public ControlledInput(Observer observer) {
+        this.inputs = new HashMap<String, Input>();
+
+        this.inputs.put(
+            InputMethods.CONSOLE,
+            new ConsoleInput()
+        );
+
+        this.inputs.put(
+            InputMethods.FILE,
+            new FileInput()
+        );
+
+        this.subscribeToInputChange(observer);
+    }
+    
+    @Override
+    public String next() {
+        return this.inputs.get(this.method).next();
+    };
+
+    @Override
+    public Integer nextInt() {
+        return this.inputs.get(this.method).nextInt();
+    };
+
+    @Override
+    public String nextLine() {
+        return this.inputs.get(this.method).nextLine();
+    };
+
+    @Override
+    public boolean hasNext(Pattern pattern) {
+        return this.inputs.get(this.method).hasNext(pattern);
+    };
+
+    @Override
+    public void trigger(String event, Object data) {
+        Pair<String, Input> parsedData = (Pair<String, Input>)data;
+        this.method = parsedData.get0();
+    }
+}
