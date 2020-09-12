7f3d8142db4ec25f0c227e90e47884dd44fbabaa
Lebed-kun <lebed-kun@mail.ru>
2020-09-12 18:48:35 +0300

    Add observer

---
+++ src/main/java/sum_machine/utils/Observable.java
@@ -0,0 +1,5 @@
+package src.main.java.sum_machine.utils;
+
+public interface Observable {
+    public void trigger();
+}
---
+++ src/main/java/sum_machine/utils/Observer.java
@@ -0,0 +1,49 @@
+package src.main.java.sum_machine.utils;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public final class Observer {
+    private HashMap<String, HashSet<Observable>> events;
+    
+    public Observer() {
+        this.events = new HashMap<
+            String,
+            HashSet<Observable> 
+        >();
+    }
+
+    public Observer subscribe(String event, Observable observable) {
+        if (!this.events.containsKey(event)) {
+            this.events.put(event, new HashSet<Observable>());
+        }
+
+        this.events.get(event).add(observable);
+
+        return this;
+    }
+
+    public Observer unsubscribe(String event, Observable observable) {
+        if (!this.events.containsKey(event)) {
+            return null;
+        }
+
+        this.events.get(event).remove(observable);
+
+        return this;
+    }
+
+    public Observer dispatch(String event) {
+        if (!this.events.containsKey(event)) {
+            return null;
+        }
+
+        HashSet<Observable> observables = this.events.get(event);
+
+        for (Observable observable : observables) {
+            observable.trigger();
+        }
+
+        return this;
+    }
+}
