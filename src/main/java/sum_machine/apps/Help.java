48dceb302e71ebd11196790af76ee8ad4d02268e
Artem Rastorguev <hizlle@mail.ru>
2020-09-20 15:52:17 +0400

    add Help manual

--- src/main/java/sum_machine/apps/Help.java
+++ src/main/java/sum_machine/apps/Help.java
@@ -4,14 +4,20 @@ import src.main.java.sum_machine.io.Output;
 
 public class Help implements Application {
     private Output output;
-    
+
     public Help(Output output) {
         this.output = output;
     }
 
     @Override
     public void run() {
-        // Your code here
+        this.output.output(
+                "summator - calculate sum of integer numbers by filter;\n" +
+                "load_to_summator - load data for \"summator\" from file;\n" +
+                "reset_summator - reset data for \"summator\" (it asks for user input again);\n" +
+                "help - output help about options;\n" +
+                "exit - terminate the program."
+        ) ;
     };
 
     @Override
